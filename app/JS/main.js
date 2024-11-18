702433631771e7aa0e287c9601c7e2a9a48c1745
WilsonD09 <wilsond34@nycstudents.net>
2024-11-18 12:32:28 -0500

    Update main.js

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,1 +1,17 @@
-import "./style.css";
+import "../style.css";
+
+async function getData() {
+  try {
+    const response = await fetch("https://valorant-api.com/v1/bundles");
+    const info = await response.json();
+    const data = info.data.slice(0, 125);
+    data.forEach((x) => {
+      console.log(x.displayName);
+    });
+  } catch (error) {
+    console.log(error);
+    alert("sorry");
+  }
+}
+
+getData();
